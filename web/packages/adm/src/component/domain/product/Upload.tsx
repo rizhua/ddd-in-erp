a3ee4ef6e388dec759df4a081b04276f3c819ea6
zhonghexi <223779899@qq.com>
2025-01-08 19:22:04 +0800

    增加许可

--- web/packages/adm/src/component/domain/product/Upload.tsx
+++ web/packages/adm/src/component/domain/product/Upload.tsx
@@ -132,13 +132,19 @@ export function Upload(props: UploadProps) {
         {fileList.map(m => <Card key={m}>
             <img src={m} alt="" />
             <div className="btn-box">
-                <a className="iconfont" onClick={e => onDelete(e, m)}>&#xe618;</a>
+                <a onClick={e => onDelete(e, m)}>
+                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+                </svg>
+                </a>
             </div>
         </Card>)}
         {fileList.length < maxCount && <Card onClick={onUpload}>
             <input type="file" ref={inputRef} hidden onChange={onFileChange} accept={accept} multiple={multiple} />
             <div className="upload-box">
-                <i className="iconfont">&#xe678;</i>
+                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+                </svg>
                 <p>上传图片</p>
             </div>
         </Card>}
